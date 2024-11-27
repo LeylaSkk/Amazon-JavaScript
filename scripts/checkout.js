48d77f522f82de9de519ef2e1303430a71f47858
LeylaSkk <leila.skouri.ls@gmail.com>
2024-11-27 07:42:45 +0100

    did the delete function in checkout.js

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,4 @@
-import { cart } from "../data/cart.js";
+import { cart, removeFromCart } from "../data/cart.js";
 import { products } from "../data/products.js";
 import { formatCurrency } from "./utils/money.js";
 
@@ -41,7 +41,7 @@ cart.forEach((cartItem)=>{
                   <span class="update-quantity-link link-primary">
                     Update
                   </span>
-                  <span class="delete-quantity-link link-primary">
+                  <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchingProduct.id}">
                     Delete
                   </span>
                 </div>
@@ -99,3 +99,12 @@ cart.forEach((cartItem)=>{
     
 });
 document.querySelector('.js-order-summary').innerHTML = cartSummaryHTML;
+//how we will know which product to delete that why we need data-attibute to look at it with id 
+document.querySelectorAll('.js-delete-link').forEach((link)=>{
+    link.addEventListener('click',()=>{
+        const productId =link.dataset.productId;
+        removeFromCart(productId);
+        console.log(cart);
+    });
+    //now how actually we will be removing this product (productID) from the cart 
+});
